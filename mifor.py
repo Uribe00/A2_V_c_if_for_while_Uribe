2a1ef030756d190b896f76a20a0de54e61c3ede5
Daniel Uribe <a.22308051281323@cbtis128.edu.mx>
2024-09-06 10:16:34 -0600

    Add files via upload

---
+++ mifor.py
@@ -0,0 +1,3 @@
+fruits = ["manzana", "libro", "hoja"] #se hace un vector en el que se agregan varios valores de cadena
+for x in fruits: #se agrega un ciclo de repeticion para que agregue las palabras
+  print(x)#se van agregando las palabras una por una
---
+++ miwhile.py
@@ -0,0 +1,4 @@
+x=1 #Se le pone un valor a la x de 1
+while x<6: #se le agrega una condicion para que se siga ejecutando hasta que 1 sea menor a 6
+    print(x)#se va imprimiendo la x
+    x=x+1 #se va sumando 1 a la x en la secuencia
