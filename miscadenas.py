9179083367ce556af86f5d457b3722c6817508fd
Daniel Uribe <a.22308051281323@cbtis128.edu.mx>
2024-09-06 09:33:42 -0600

    Add files via upload
    subí 2 archivos

---
+++ miscadenas.py
@@ -0,0 +1,25 @@
+'''Daniel Uribe
+trabajando con cadenas
+'''
+
+nombre= "Uribe"
+print(nombre[0]) #toma el caracter 
+print(nombre)
+
+MisNovias=["U, R, I"]
+for chicas in MisNovias:
+ print(chicas)
+
+ print("otro ejemplo")
+ 
+ b = "Daniel Uribe"
+print(b[0:15])
+print("a mayusculas")
+a = "Daniel Uribe"
+print(a.upper())
+print("Concatenar")
+a = "Daniel "
+b = "Uribe"
+c = a + b
+print(c)
+
---
+++ mivariable.py
@@ -0,0 +1,19 @@
+import math
+
+edad=16 # 16 se guarda en edad 
+
+nombre="Daniel Uribe"
+print("tu edad es", edad)
+print("Te llamas", nombre)
+print("-- tipo de variables ---")
+x = str(nombre)    # x will be '3'
+y = int(3.1416)    # y will be 3
+z = float(7)  # z will be 3.0
+print(x)
+print(y)
+print(z)
+print("-- funciones matematicas ----")
+
+x = math.sqrt(64) # Funcion para sacar raiz cuadrada, lo convirte en flooat
+
+print(x)
